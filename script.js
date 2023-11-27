1bd25a532ba923e593bc13b695a20208499fe394
uneeth-Gaikwad <puneethgaikwadl@gmail.com>
2023-11-27 15:54:16 +0530

    added menu

--- script.js
+++ script.js
@@ -1,22 +1,31 @@
-try {
-    document.addEventListener('DOMContentLoaded', function () {
-        // Get all menu items
-        const menuItems = document.querySelectorAll('.menu-item');
-      
-        // Add click event listener to each menu item
-        menuItems.forEach(function (menuItem) {
-          menuItem.addEventListener('click', function () {
-            // Remove 'active' class from all menu items
-            menuItems.forEach(function (item) {
-              item.classList.remove('active');
-            });
-      
-            // Add 'active' class to the clicked menu item
-            menuItem.classList.add('active');
-          });
-        });
+document.addEventListener('DOMContentLoaded', function () {
+  const menuItems = document.querySelectorAll('.menu-item');
+  const contentDivs = document.querySelectorAll('.content');
+
+  menuItems.forEach(function (menuItem) {
+    menuItem.addEventListener('click', function (event) {
+      // Prevent the default behavior (scrolling to the top)
+      event.preventDefault();
+
+      // Remove 'active' class from all menu items
+      menuItems.forEach(function (item) {
+        item.classList.remove('active');
       });
-} catch (error) {
-    console.error(error);
-}
-  
+
+      // Remove 'active' class from all content divs
+      contentDivs.forEach(function (contentDiv) {
+        contentDiv.classList.remove('active');
+      });
+
+      // Add 'active' class to the clicked menu item
+      menuItem.classList.add('active');
+
+      // Get the target div associated with the clicked menu item
+      const targetDivId = menuItem.getAttribute('data-target');
+      const targetDiv = document.getElementById(targetDivId);
+
+      // Add 'active' class to the target div
+      targetDiv.classList.add('active');
+    });
+  });
+});
